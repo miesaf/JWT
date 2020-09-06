40e2522b88bdfa0922f7fac0b7a56ef357545a62
Muhamad Amirul <19381137+miesaf@users.noreply.github.com>
2020-09-06 13:21:39 +0800

    Auth : API register new user

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -51,7 +51,7 @@ class RegisterController extends Controller
     {
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
-            'username' => ['required', 'string', 'max:255'],
+            'username' => ['required', 'string', 'max:255', 'unique:pengguna'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:pengguna'],
             'role' => ['required', 'in:user,admin'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
